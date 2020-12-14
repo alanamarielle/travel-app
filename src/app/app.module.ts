ef4c796bba25c53b7171ce4180f4fad982269d46
Alana Frank <alanamarielle@gmail.com>
2020-12-14 12:09:21 -0500

    added about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { GalleriaModule } from 'primeng/galleria';
 import { AppComponent } from './app.component';
 import { CityComponent } from './city/city.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { WeatherComponent } from './weather/weather.component';
 import { PhotosComponent } from './photos/photos.component';
 import { BooksComponent } from './books/books.component';
@@ -24,6 +25,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     MusicComponent,
     RecipesComponent,
     PageNotFoundComponent,
+    AboutComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, GalleriaModule],
   providers: [],
